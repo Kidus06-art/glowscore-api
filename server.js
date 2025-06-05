00eb3c0bb7484977653ce8e3876119d541b8e8f4
Kidus06-art <kidusd758@gmail.com>
2025-06-04 22:40:44 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config();
+
+const glowRoute = require('./index'); // or replace with your route filename
+
+app.use(bodyParser.json());
+app.use('/api', glowRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
